46ca890de01df008d06e8e88e20be3555d954fc9
empi91 <korpet.filip@gmail.com>
2025-11-26 16:07:27 +0100

    Adding database methods

--- app/database/database.py
+++ app/database/database.py
@@ -3,30 +3,38 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import config
 from app.models import Base
-from app.schemas import Plane
+from app.schemas import Plane as PydanticPlane
 
 
 class Database:
     def __init__(self):
         """Declaration of database object, used for all database operations"""
-        # TODO Check if database exists, if YES continue, if NO create new database
+        # ADDFEATURE Check if database exists, if YES continue, if NO create new database
 
-    def add_plane(self, plane: Plane):
-        """Adding new plane to database"""
+        self.engine = create_engine(config.database.database_engine)
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+
+    def add_plane(self, plane: PydanticPlane):
+        """
+        Adding new plane to database
+        Converting pydantic plane model to ORM plane model
+        Commiting created plane to database
+        """
         # ADDFEATURE Adding new plane to database
 
-    def get_plane(self):
-        """Getting plane data from database"""
+    def get_all_planes(self) -> list[PydanticPlane]:
+        """
+        Getting all planes data from database
+        Converting them back to pydantic models and returning it in a list
+        """
         # ADDFEATURE Getting plane from DB every second (?) to update it's position
+        return []
 
-    def update_plane(self):
-        """Updating plane data in database"""
+    def update_plane(self, plane: PydanticPlane):
+        """
+        Updating plane data in database
+        Getting ORM plane model from database
+        Updating all ORM fields with values passed to method inside pydantic plane
+        """
         # ADDFEATURE Updating plane's data in database after each time period
-
-
-engine = create_engine(config.database.database_engine)
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-with Session() as session:
-    pass
